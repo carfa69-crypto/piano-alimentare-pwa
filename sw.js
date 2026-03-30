ae4c61814760fe9c3b134a9b18951cf38f1a5b54
carfa69-crypto <carfa69@gmail.com>
2026-03-30 18:28:03 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+// Service Worker per Piano Alimentare PWA
+const CACHE_NAME = 'piano-alimentare-v1';
+const urlsToCache = [
+  '/piano-alimentare-pwa.html',
+  '/manifest.json'
+];
+
+// Installazione
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Attivazione
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch - Strategia: Cache first, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
